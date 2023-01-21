c54453b6e780ca78e219d7e4979d6a014e37c717
jhrudd <johnhenryrudden@gmail.com>
2023-01-21 16:56:46 -0600

    Add simple stream function

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 from djitellopy import Tello
+import cv2
+import imutils
+
+
+
 
 class TelloController: 
+    tello = None;
     def __init__(self):
-        tello = Tello()
+        self.tello = Tello()
+        self.tello.connect()
+    
+    def start_stream(self):
+        '''Turns on tello stream settings and returns frame stream'''
+        self.tello.streamon()
+        return self.tello.get_frame_read()
 
-        tello.connect()
-        tello.takeoff()
+    def end(self):
+        self.tello.streamoff()
 
-        tello.move_left(100)
-        tello.rotate_counter_clockwise(90)
-        tello.move_forward(100)
+    def run_stream(self):
+        cv2.startWindowThread()
+        frame_reader = self.start_stream();
+        while(True):
+            img = frame_reader.frame
 
-        tello.land()
+            img = cv2.resize(img, (1920, 1080))
+            cv2.imshow('Image', img)
 
+            if cv2.waitKey(1)& 0xFF == ord('q'):
+                break
+        self.end()
+        cv2.destroyAllWindows()
+
+
+TelloController().run_stream()
