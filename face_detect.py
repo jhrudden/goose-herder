754c43fff3b5fcffbe4194a3fcc99d7f8b11ad20
John Henry Rudden <johnhenryrudden@gmail.com>
2023-02-13 22:39:30 -0500

    Add distance to frame

--- face_detect.py
+++ face_detect.py
@@ -1,24 +1,49 @@
 import cv2
+from .goosey import calculate_distance 
 
+MAC_M1_FOCAL_LENGTH = 50.0  # in millimeters
+JH_FACE_HEIGHT = 209.55 # in millimeters
 
 class FaceDetector:
     def __init__(self):
         # setup face classifier
         face_cascade_path = 'data/haarcascade_frontalface_default.xml'
-        face_cascade = cv2.CascadeClassifier()
-        if not face_cascade.load(face_cascade_path):
+        self.face_cascade = cv2.CascadeClassifier()
+        if not self.face_cascade.load(face_cascade_path):
             print(f"Failed to open haarcascade file: {face_cascade_path}")
             exit(0)
+    
+    def draw_face_distance(self, frame, face):
+        (x, y, w, h) = face
+        face_height_in_frame = abs(y - h) 
+        img_height = frame.shape[0]
+        print(f'Image Hieght: {str(img_height)}')
+        dist = calculate_distance(img_height, JH_FACE_HEIGHT, face_height_in_frame)
+        return cv2.putText(frame, f'{str(dist)}mm', [x + (w // 2), y + (h // 2)], cv2.FONT_HERSHEY_SIMPLEX,
+            1, (255, 0, 0), 2, cv2.LINE_AA)
+        
 
-    def detect_faces_in_frame(self, frame):
+
+    def draw_faces(self, frame, faces):
+        for face in faces:
+            (x, y, w, h) = face
+            center = (x + w//2, y + h//2)
+            frame = cv2.rectangle(frame, [x, y], [x+w, y+h], (255, 0, 0), 2)
+            frame = self.draw_face_distance(frame, face)
+        return frame
+        
+
+    def detect_faces(self, frame):
         frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame_grey = cv2.equalizeHist(frame_grey)
 
-        faces = face_cascade.detectMultiScale(frame_grey)
-        for (x, y, w, h) in faces:
-            center = (x + w//2, y + h//2)
-            frame = cv2.rectangle(frame, [x, y], [x+w, y+h], (255, 0, 0), 2)
-        cv2.imshow('frame', frame)
+        faces = self.face_cascade.detectMultiScale(frame_grey)
+        return faces
+
+    def detect_and_draw(self, frame):
+        faces = self.detect_faces(frame)
+        return self.draw_faces(faces)
+
 
 
 if __name__ == '__main__':
