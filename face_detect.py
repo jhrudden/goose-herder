e663aaf6d0298a6e13005eb5c4450400a1fd7c53
John Henry Rudden <johnhenryrudden@gmail.com>
2023-02-21 21:41:24 -0500

    closer to distance

--- face_detect.py
+++ face_detect.py
@@ -1,8 +1,12 @@
 import cv2
-from .goosey import calculate_distance 
 
 MAC_M1_FOCAL_LENGTH = 50.0  # in millimeters
 JH_FACE_HEIGHT = 209.55 # in millimeters
+
+# Referenced: https://photo.stackexchange.com/questions/12434/how-do-i-calculate-the-distance-of-an-object-in-a-photo
+def calculate_distance(image_height: float, known_object_height: float, current_height: float, focal_length: float = MAC_M1_FOCAL_LENGTH ) -> float:
+    return (focal_length * known_object_height) / current_height
+
 
 class FaceDetector:
     def __init__(self):
@@ -15,15 +19,14 @@ class FaceDetector:
     
     def draw_face_distance(self, frame, face):
         (x, y, w, h) = face
-        face_height_in_frame = abs(y - h) 
+        face_height_in_frame = abs(y - h)
+        print(f'height camera {face_height_in_frame}')
         img_height = frame.shape[0]
-        print(f'Image Hieght: {str(img_height)}')
         dist = calculate_distance(img_height, JH_FACE_HEIGHT, face_height_in_frame)
-        return cv2.putText(frame, f'{str(dist)}mm', [x + (w // 2), y + (h // 2)], cv2.FONT_HERSHEY_SIMPLEX,
+        dist_ft = int(dist * 0.00328084)
+        return cv2.putText(frame, f'{str(dist_ft)}ft', [x + (w // 2), y + (h // 2)], cv2.FONT_HERSHEY_SIMPLEX,
             1, (255, 0, 0), 2, cv2.LINE_AA)
         
-
-
     def draw_faces(self, frame, faces):
         for face in faces:
             (x, y, w, h) = face
@@ -31,18 +34,18 @@ class FaceDetector:
             frame = cv2.rectangle(frame, [x, y], [x+w, y+h], (255, 0, 0), 2)
             frame = self.draw_face_distance(frame, face)
         return frame
-        
 
     def detect_faces(self, frame):
         frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame_grey = cv2.equalizeHist(frame_grey)
 
         faces = self.face_cascade.detectMultiScale(frame_grey)
+        print(faces)
         return faces
 
     def detect_and_draw(self, frame):
         faces = self.detect_faces(frame)
-        return self.draw_faces(faces)
+        return self.draw_faces(frame, faces)
 
 
 
@@ -57,7 +60,8 @@ if __name__ == '__main__':
 
     while True:
         ret, frame = video_capture.read()
-        fd.detect_faces_in_frame(frame)
+        fd.detect_and_draw(frame)
 
-        if cv2.waitKey(30) & 0xFF == ord('q'):
+        if cv2.waitKey(100) & 0xFF == ord('q'):
             break
+        cv2.imshow('frame', frame)
--- goosey.py
+++ goosey.py
@@ -1,11 +1,7 @@
-from tello_controller import TelloController 
+from tello_controller import TelloController
 import cv2
 
 TELLO_FOCAL_LENGTH = 3.0
-
-# Referenced: https://photo.stackexchange.com/questions/12434/how-do-i-calculate-the-distance-of-an-object-in-a-photo
-def calculate_distance(image_height: int, known_object_height: float, current_height: int, focal_length: float = MAC_M1_FOCAL_LENGTH ) -> float:
-    return (focal_length * image_height * current_height) / known_object_height
 
 
 if __name__ == '__main__':
